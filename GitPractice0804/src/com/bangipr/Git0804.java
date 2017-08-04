9d08c250c283dc01fba12ed253db392aae981a09
BANGIPR <BANGIPR@ZHA-ITA002-W7.corp.oocl.com>
2017-08-04 11:58:57 +0800

    master

--- GitPractice0804/src/com/bangipr/Git0804.java
+++ GitPractice0804/src/com/bangipr/Git0804.java
@@ -5,6 +5,7 @@ public class Git0804 {
 	public static void main(String[] args) {
 		
 		System.out.println("Hannah");
+		System.out.println("Hello");
 		System.out.println("January 21, 1996");
 		System.out.println("1233");
 		
