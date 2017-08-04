c784da890603f2c3e792351ec303ae6b4fd4169a
BANGIPR <BANGIPR@ZHA-ITA002-W7.corp.oocl.com>
2017-08-04 11:27:48 +0800

    First Push

---
+++ GitPractice0804/src/com/bangipr/Git0804.java
@@ -0,0 +1,13 @@
+package com.bangipr;
+
+public class Git0804 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hannah");
+
+	}
+
+}
